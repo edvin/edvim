b0d30b41cd145c2c5d94374ef8a182e47276d5c3
Edvin Syse <edsy@one.com>
2023-08-06 23:52:29 +0200

    Disable tab custom cmp, not needed at all, default nvim is much better!

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -76,29 +76,6 @@ return {
           ['<C-Space>'] = cmp.mapping.complete(),
           ['<C-e>'] = cmp.mapping.abort(),
           ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-
-          ['<Tab>'] = function(fallback)
-            if is_blank_line() then
-              fallback()
-            elseif vim.fn.pumvisible() == 1 then
-              vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-n>', true, true, true), 'n')
-            elseif luasnip.expand_or_jumpable() then
-              vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
-            elseif not check_backspace() then
-              cmp.mapping.complete()(fallback)
-            else
-              fallback()
-            end
-          end,
-          ['<S-Tab>'] = function(fallback)
-            if vim.fn.pumvisible() == 1 then
-              vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-p>', true, true, true), 'n')
-            elseif luasnip.jumpable(-1) then
-              vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
-            else
-              fallback()
-            end
-          end,
         }),
         sources = cmp.config.sources({
           { name = 'nvim_lsp_signature_help' },
