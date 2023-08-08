86399770497b8d0955979ee9a95f69329c026ab9
Edvin Syse <edsy@one.com>
2023-08-08 10:05:08 +0200

    Removed duplicate definition of cmp, deleted code.lua

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,4 +1,5 @@
 return {
+  { "onsails/lspkind-nvim", },
   { 'hrsh7th/cmp-buffer', },
   { 'hrsh7th/cmp-cmdline', },
   { 'hrsh7th/cmp-path', },
@@ -21,6 +22,25 @@ return {
       require("luasnip.loaders.from_vscode").lazy_load({
         paths = "~/.config/nvim/snippets/"
       })
+
+      -- Completion keymaps:
+      --
+      -- <Ctrl-K> for expanding,
+      -- <Ctrl-L> for jumping forward
+      -- <Ctrl-J> for jumping backward
+      -- <Ctrl-E> for changing the active choice
+      --
+      local ls = require('luasnip')
+
+      vim.keymap.set({ "i" }, "<C-K>", function() ls.expand() end, { silent = true })
+      vim.keymap.set({ "i", "s" }, "<C-L>", function() ls.jump(1) end, { silent = true })
+      vim.keymap.set({ "i", "s" }, "<C-J>", function() ls.jump(-1) end, { silent = true })
+
+      vim.keymap.set({ "i", "s" }, "<C-E>", function()
+        if ls.choice_active() then
+          ls.change_choice(1)
+        end
+      end, { silent = true })
     end
   },
   {
@@ -33,12 +53,14 @@ return {
       'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-path',
       'hrsh7th/cmp-cmdline',
+      "onsails/lspkind-nvim",
       'hrsh7th/cmp-nvim-lua',
     },
     config = function()
       vim.opt.completeopt = { "menu", "menuone", "noselect" }
 
       local luasnip = require('luasnip')
+      local lspkind = require('lspkind')
       local cmp = require 'cmp'
 
       local check_backspace = function()
@@ -50,12 +72,6 @@ return {
         return vim.api.nvim_get_current_line():match("^%s*"):len() == 0
       end
 
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
-
       cmp.setup({
         snippet = {
           expand = function(args)
@@ -64,24 +80,54 @@ return {
         },
 
         formatting = {
-          format = require("tailwindcss-colorizer-cmp").formatter
+          format = lspkind.cmp_format(),
+          -- format = require("tailwindcss-colorizer-cmp").formatter
         },
         window = {
           completion = cmp.config.window.bordered(),
           documentation = cmp.config.window.bordered(),
         },
-        mapping = cmp.mapping.preset.insert({
+        mapping = {
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-k>"] = cmp.mapping.select_prev_item(),
+          ["<C-j>"] = cmp.mapping.select_next_item(),
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-Space>'] = cmp.mapping.complete(),
           ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-        }),
+          ['<CR>'] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Insert,
+            select = true
+          }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          --   ['<Tab>'] = function(fallback)
+          --     if is_blank_line() then
+          --       fallback()
+          --     elseif vim.fn.pumvisible() == 1 then
+          --       vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-n>', true, true, true), 'n')
+          --     elseif luasnip.expand_or_jumpable() then
+          --       vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
+          --     elseif not check_backspace() then
+          --       cmp.mapping.complete()(fallback)
+          --     else
+          --       fallback()
+          --     end
+          --   end,
+          --   ['<S-Tab>'] = function(fallback)
+          --     if vim.fn.pumvisible() == 1 then
+          --       vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-p>', true, true, true), 'n')
+          --     elseif luasnip.jumpable(-1) then
+          --       vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
+          --     else
+          --       fallback()
+          --     end
+          --   end,
+        },
         sources = cmp.config.sources({
           { name = 'nvim_lsp_signature_help' },
           { name = 'nvim_lsp' },
-          { name = 'vim-dadbod-completion' },
           { name = 'luasnip' },
+          { name = 'vim-dadbod-completion' },
         }, {
           { name = 'buffer' },
         })
