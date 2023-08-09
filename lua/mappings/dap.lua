e388e8381665c8b4f7676055ba860c606e0e09db
Edvin Syse <977035+edvin@users.noreply.github.com>
2023-08-09 23:00:36 +0200

    Added debugging shortcuts

--- lua/mappings/dap.lua
+++ lua/mappings/dap.lua
@@ -1,2 +1,9 @@
-vim.keymap.set('n', '<F5>', ':lua require("dap").continue()<CR>', { desc = "Toggle breakpoint", silent = true })
-vim.keymap.set('n', '<F9>', ':lua require("dap").toggle_breakpoint()<CR>', { desc = "Toggle breakpoint", silent = true })
+-- TODO: Configure terminal or nvim remapping to support Ctrl+F8, Shift-F8 and Alt-F8
+vim.keymap.set('n', '<A-F8>', ':lua require("dap").eval()<CR>', { desc = "Evaluate symbol under cursor", silent = true })
+vim.keymap.set('n', '<F9>', ':lua require("dap").continue()<CR>', { desc = "Toggle breakpoint", silent = true })
+vim.keymap.set('n', '<F7>', ':lua require("dap").step_over()<CR>', { desc = "Step over", silent = true })
+vim.keymap.set('n', '<F8>', ':lua require("dap").step_into()<CR>', { desc = "Step into", silent = true })
+vim.keymap.set('n', '<S-F8>', ':lua require("dap").step_out()<CR>', { desc = "Step out", silent = true })
+vim.keymap.set('n', '<C-F8>', ':lua require("dap").toggle_breakpoint()<CR>',
+  { desc = "Toggle breakpoint", silent = true })
+vim.keymap.set('n', '<F11>', ':lua require("dapui").toggle()<CR>', { desc = "Toggle debugger UI", silent = true })
