0726b89ef4037a303600cb22491e5eae7f7ac609
Edvin Syse <edsy@one.com>
2023-08-09 13:37:08 +0200

    Add DAP UI

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -11,6 +11,27 @@ return {
     end
   },
   {
+    'theHamsta/nvim-dap-virtual-text',
+    config = function()
+      require("nvim-dap-virtual-text").setup()
+    end
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "folke/neodev.nvim"
+    },
+    config = function()
+      require("dapui").setup()
+
+      -- Enable type checking for nvim-dap-ui to get type checking, documentation and autocompletion for all API functions
+      require("neodev").setup({
+        library = { plugins = { "nvim-dap-ui" }, types = true },
+      })
+    end
+  },
+  {
     "leoluz/nvim-dap-go",
     config = function()
       require('dap-go').setup {
