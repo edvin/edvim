8c279eb13863bfb3f1b0a213a420afbd53d45464
Edvin Syse <edvin@syse.priv.no>
2023-08-13 00:05:43 +0200

    Added prettier

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,6 +1,46 @@
 return {
   {
-    "jose-elias-alvarez/null-ls.nvim"
+    "MunifTanjim/prettier.nvim",
+    config = function()
+      require('prettier').setup({
+        bin = 'prettierd',
+        filetypes = {
+          "css",
+          "graphql",
+          "html",
+          "javascript",
+          "json",
+          "less",
+          "markdown",
+          "scss",
+          "typescript",
+          "yaml",
+        },
+        cli_options = {
+          arrow_parens = "always",
+          bracket_spacing = true,
+          bracket_same_line = false,
+          embedded_language_formatting = "auto",
+          end_of_line = "lf",
+          html_whitespace_sensitivity = "css",
+          -- jsx_bracket_same_line = false,
+          jsx_single_quote = false,
+          print_width = 140,
+          prose_wrap = "preserve",
+          quote_props = "as-needed",
+          semi = true,
+          single_attribute_per_line = false,
+          single_quote = false,
+          tab_width = 2,
+          trailing_comma = "es5",
+          use_tabs = false,
+          vue_indent_script_and_style = false,
+        },
+      })
+    end
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
   },
   {
     "jay-babu/mason-null-ls.nvim",
@@ -11,7 +51,7 @@ return {
     },
     config = function()
       require("mason-null-ls").setup({
-        ensure_installed = { "stylua" }
+        ensure_installed = { "stylua", "prettierd" }
       })
     end,
   },
