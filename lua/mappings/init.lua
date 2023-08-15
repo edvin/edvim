4ddf6460bc304f1a25dc20232d48e7027fdae909
Edvin Syse <edsy@one.com>
2023-08-15 13:33:58 +0200

    Silenced tab jump

--- lua/mappings/init.lua
+++ lua/mappings/init.lua
@@ -15,8 +15,8 @@ vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { desc = "Toggle file ex
 vim.keymap.set('n', '<leader>x', ':bd<CR>:bprev<CR>', { desc = "Close current buffer" })
 
 -- Cycle through buffers
-vim.keymap.set('n', '<Tab>', ':bnext<CR>', { desc = "Next buffer" })
-vim.keymap.set('n', '<s-Tab>', ':bprev<CR>', { desc = "Previous buffer" })
+vim.keymap.set('n', '<Tab>', ':bnext<CR>', { desc = "Next buffer", silent = true })
+vim.keymap.set('n', '<s-Tab>', ':bprev<CR>', { desc = "Previous buffer", silent = true })
 
 -- Organize imports
 vim.keymap.set('n', '<leader>ci',
