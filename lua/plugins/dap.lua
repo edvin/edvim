37731ba236268d140ef2f9e2e79ef0cdbe21865c
Edvin Syse <edsy@one.com>
2023-08-15 13:08:31 +0200

    Update debug shortcuts

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -35,14 +35,6 @@ return {
     "leoluz/nvim-dap-go",
     config = function()
       require('dap-go').setup {
-        dap_configurations = {
-          {
-            type = "go",
-            name = "Attach remote",
-            mode = "remote",
-            request = "attach",
-          }
-        },
         delve = {
           path = "dlv",
           initialize_timeout_sec = 20,
