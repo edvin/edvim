24c864d90301640f06465db7c90c9ac741a6e5aa
Edvin Syse <977035+edvin@users.noreply.github.com>
2023-08-19 08:55:36 +0200

    Added helpful telescope shortcuts

--- lua/mappings/telescope.lua
+++ lua/mappings/telescope.lua
@@ -5,3 +5,11 @@ vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Find buffers" })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Find in help" })
 vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, { desc = "Document symbols" })
 vim.keymap.set('n', '<leader>fw', builtin.lsp_dynamic_workspace_symbols, { desc = "Workspace symbols" })
+vim.keymap.set('n', '<leader>/', function()
+  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+    winblend = 10,
+    previewer = false,
+  })
+end, { desc = "Fuzzy search current buffer" })
+vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = "Find existing buffers" })
+vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = "Find recently open files" })
