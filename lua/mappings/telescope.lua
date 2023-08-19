bcd899095beb2a6e7da8fa404b2696385340b146
Edvin Syse <977035+edvin@users.noreply.github.com>
2023-08-19 23:25:48 +0200

    Allow better space for telescope symbol results

--- lua/mappings/telescope.lua
+++ lua/mappings/telescope.lua
@@ -3,8 +3,18 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find files" })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Live grep" })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Find buffers" })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Find in help" })
-vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, { desc = "Document symbols" })
-vim.keymap.set('n', '<leader>fw', builtin.lsp_dynamic_workspace_symbols, { desc = "Workspace symbols" })
+vim.keymap.set('n', '<leader>fs', function()
+  builtin.lsp_document_symbols {
+    fname_width = 35,
+    symbol_width = 50
+  }
+end, { desc = "Document symbols" })
+vim.keymap.set('n', '<leader>fw', function()
+  builtin.lsp_dynamic_workspace_symbols {
+    fname_width = 35,
+    symbol_width = 50
+  }
+end, { desc = "Workspace symbols" })
 vim.keymap.set('n', '<leader>/', function()
   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
     winblend = 10,
