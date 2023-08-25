42b708026d4784a7f26e595c2db828def38019db
Edvin Syse <977035+edvin@users.noreply.github.com>
2023-08-25 17:45:56 +0200

    Add debug icons

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,6 +1,15 @@
 return {
   {
-    "mfussenegger/nvim-dap"
+    "mfussenegger/nvim-dap",
+    config = function()
+      vim.fn.sign_define('DapBreakpoint', { text = '', texthl = 'DapBreakpoint' })
+      vim.fn.sign_define('DapBreakpointCondition', { text = ' ﳁ', texthl = 'DapBreakpoint' })
+      vim.fn.sign_define('DapBreakpointRejected', { text = ' ', texthl = 'DapBreakpoint' })
+      vim.fn.sign_define('DapLogPoint', { text = ' ', texthl = 'DapLogPoint' })
+      vim.fn.sign_define('DapStopped', { text = ' ', texthl = 'DapStopped' })
+
+      vim.api.nvim_set_hl(0, 'DapBreakpoint', { ctermbg = 0, fg = '#ff0000' })
+    end
   },
   {
     "jay-babu/mason-nvim-dap.nvim",
