4c9ce53936de11a06416bcaedc2cad01982d3c25
Edvin Syse <es@syse.no>
2023-08-31 12:56:30 +0200

    Remove nav plugin, not needed, just learn vim motions, man!

--- lua/plugins/nav.lua
+++
@@ -1,9 +0,0 @@
-return {
-  -- Quick navigation using s and S followed by target letters
-  {
-    "ggandor/leap.nvim",
-    config = function()
-      require("leap").add_default_mappings()
-    end
-  },
-}
